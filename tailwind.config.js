d8427f4b50eb2141956573056c1816ded4636075
puneethkumar-v <pk.puni872@gmail.com>
2021-11-22 16:44:14 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      custom: ["Outfit", "sans-serif"],
+    },
+    extend: {},
+    colors: {
+      softBlue: "hsl(215, 51%, 70%)",
+      cyan: "hsl(178, 100%, 50%)",
+      main: "hsl(217, 54%, 11%)",
+      card: "hsl(216, 50%, 16%)",
+      line: "hsl(215, 32%, 27%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
